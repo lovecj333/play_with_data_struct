bdad382028e1d197ba206c658398a2e0acb836fc
chenjian <chenjian_336@126.com>
2019-03-10 12:29:15 +0800

    test

--- src/com/datastruct/array/Array.java
+++ src/com/datastruct/array/Array.java
@@ -1,6 +1,53 @@
 package com.datastruct.array;
 
-public class Array {
+public class Array<E> {
 
+    private E[] data;
+    private int size;
 
+    // 构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认数组的容量capacity=10
+    public Array(){
+        this(10);
+    }
+
+    // 获取数组的容量
+    public int getCapacity(){
+        return data.length;
+    }
+
+    // 获取数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 在index索引的位置插入一个新元素e
+    public void add(int index, E e){
+        if(index < 0 || index >= size){
+            throw new RuntimeException("就想问问你,你的index怎么传的");
+        }
+        if(size == data.length){
+            resize(2 * data.length);
+        }
+
+    }
+
+    // 将数组空间的容量变成newCapacity大小
+    private void resize(int newCapacity){
+        E[] newData = (E[])new Object[newCapacity];
+        for(int i=0;i<size;i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
 }
