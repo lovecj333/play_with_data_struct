889175406b98affcd02c8f92763759479201d238
chenjian <chenjian_336@126.com>
2019-03-11 14:08:24 +0800

    queue

---
+++ src/com/datastruct/queue/Queue.java
@@ -0,0 +1,14 @@
+package com.datastruct.queue;
+
+public interface Queue<E> {
+
+    public int getSize();
+
+    public boolean isEmpty();
+
+    public void enqueue(E e);
+
+    public E dequeue();
+
+    public E getFront();
+}
