f76ce4f4a6d8b0beb232d02d61c7979eb13f74df
chenjian <chenjian_336@126.com>
2019-03-13 13:01:13 +0800

    linkedliststack

--- src/com/datastruct/stack/TestStack.java
+++ src/com/datastruct/stack/TestStack.java
@@ -3,7 +3,8 @@ package com.datastruct.stack;
 public class TestStack {
 
     public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
+        //ArrayStack<Integer> stack = new ArrayStack<>();
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
         for(int i = 0;i<5;i++){
             stack.push(i);
             System.out.println(stack);
